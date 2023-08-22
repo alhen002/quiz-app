411ea58e1d47aa38d513a3a7d882c23373b4b075
Alexander Henting <a.henting@posteo.de>
2023-08-22 12:15:34 +0200

    js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const allCards = document.querySelectorAll("article");
 
-console.log(allCards);
+
 allCards.forEach((article) => {
   article.querySelector(":scope button").addEventListener("click", () => {
     article.querySelector(":scope p").classList.toggle("hidden");
